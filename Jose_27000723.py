3cccb28f3f57e868626e10b347855cc5db4b42d8
josemvegar <josevega1999.16@gmail.com>
2019-11-06 19:21:58 -0400

    listo

--- Jose_27000723.py
+++ Jose_27000723.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 """
 Proyecto Algoritmo de Newton-Raphson.
 
@@ -32,12 +32,45 @@ def newton_raphson(f, x, ER, N):
     ER: cota mínima del error relativo.
     N: número máximo de iteraciones.
     """
-
-    print("Iteración:", i, "Aproximación:", xi, "Error:", err)
-
+    i= 0
+    xi=0
+    from math import e
+    while (N> i):
+        fund=derivada(f)
+        xi=x-(f(x)/fund(x))
+        i=i+1
+        err=(xi-x)/xi
+        x=xi
+        if (err<0):
+            err=err*-1
+        print("Iteración:", i, "Aproximación:", xi, "Error:", err)
+        if (err<ER):
+            i=N
     return xi
-
-
+f = lambda x: math.cos(x)- (x)  
 if __name__ == "__main__":
-    # Pruebe aquí su función.
+    rep="s"
+    while (rep=="s"):
+        print ("Algoritmo de Newton Raphson para aproximar las raices de una función mediante derivadas.")
+        print ("ingrese el valor de la aproximación inicial")
+        x= float (input ())
+        print ("Ingrese el valor del error para d1etener el algoritmo")
+        err=float (input ())
+        while (err<0):
+            print ("El error no puede ser menor a 0, ingrese un valor correcto.")
+            err= float (input ())
+        print ("¿Cuántas veces se va a iterar si no ha llegado al error?")
+        n= float (input ())
+        while (n< 1):
+            print ("La cantidad de iteraciones no puede ser menor a 1, ingrese un valor correcto")
+            n= input ()
+        #print ("el punto medio más cercano a la raiz es: ",biseccion(a, b, err, n,0,0,0,0))
+        print ("la aproximación más cercana a la raiz es: ",newton_raphson(f, x, err, n))
+        print ("fin del algoritmo, desea repetir las operacones (n/s) ?")
+        rep= input ()
+        while (rep != "n" and rep != "s"):
+            print ("Ingrese un valor correcto (n/s)")
+            rep= input()
+    print ("Bye.")
+    chao=input()
     pass
