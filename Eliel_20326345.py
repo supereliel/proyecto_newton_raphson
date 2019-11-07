b2c4fa0ac56b4be606284e090bbcd90032eb1c0e
Eliel <supereliel15@gmail.com>
2019-11-06 23:58:57 -0400

    implementación algoritmo

--- Eliel_20326345.py
+++ Eliel_20326345.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Proyecto Algoritmo de Newton-Raphson.
-
-Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 """
 
 import math
@@ -32,12 +31,30 @@ def newton_raphson(f, x, ER, N):
     ER: cota mínima del error relativo.
     N: número máximo de iteraciones.
     """
-
-    print("Iteración:", i, "Aproximación:", xi, "Error:", err)
-
-    return xi
-
+    error_aprox  = ER+1000000
+    aprox_actual = x
+    i = 1
+    while i <= N:
+        if error_aprox > ER:
+            fp = derivada(f)(aprox_actual)
+            if fp == 0:
+                print("El método no se puede aplicar, el resultado de la derivada es 0.")
+                return 0
+            aprox_sig    = aprox_actual - (f(aprox_actual)/fp)
+            error_aprox  = math.fabs((aprox_sig-aprox_actual)/aprox_sig)
+            aprox_actual = aprox_sig
+            print("Iteración:", i,"Aproximación:", aprox_actual,"Error:", error_aprox)
+        i += 1
+    return aprox_actual
 
 if __name__ == "__main__":
-    # Pruebe aquí su función.
-    pass
+    ITERACIONES = 4
+    ER          = 0.02
+
+    f = lambda x: math.exp(-x)-math.log(x)
+    g = lambda x: math.atan(x)+x-1
+    y = lambda x: math.pow(x,2)-26
+
+    newton_raphson(f, 1, ER, ITERACIONES)
+    #newton_raphson(g, 0, ER, ITERACIONES)
+    #newton_raphson(y, 5, ER, ITERACIONES)
